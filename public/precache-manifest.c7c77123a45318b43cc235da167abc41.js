e8b5603f1dbbc724707c13cf8967e3825548928d
Manon Morgaut <manonmorgaut@gmail.com>
2020-08-06 01:21:22 +0200

    Commit for heroku

---
+++ public/precache-manifest.c7c77123a45318b43cc235da167abc41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e6fdf19d9c3d9841c4b04d013db017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4779e4284bf817130eea",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "8bfe26d8ce1e2ba19ba7",
+    "url": "/static/css/main.e2ea4edb.chunk.css"
+  },
+  {
+    "revision": "4779e4284bf817130eea",
+    "url": "/static/js/2.97a36f7f.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.97a36f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfe26d8ce1e2ba19ba7",
+    "url": "/static/js/main.8622b8cf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
